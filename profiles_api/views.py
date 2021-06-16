bcf245f9e08751f784aa6a0a8152dd6465db4e25
WilliamWYK <coold030b@gmail.com>
2021-06-16 13:32:44 +0800

    Create API GET/POST/PUT/PATCH/DELETE Methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from rest_framework import status
+from profiles_api import serializers
+
 class HelloAPIView(APIView):
     """Test api view"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -14,3 +18,30 @@ class HelloAPIView(APIView):
         ]
         
         return Response({'message':'Hello','an_apiview':an_apiview})
+
+    def post(self,request):
+        """Create a hello message with our post name"""
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+                )
+    
+    def put(self, request,pk=None):
+        """Update(replace) with a new object"""
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        """Partial Update with object fields"""
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
+
