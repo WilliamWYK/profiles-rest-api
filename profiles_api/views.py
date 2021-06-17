807a49bc73f71767b4c52403988724bb20cc0028
WilliamWYK <coold030b@gmail.com>
2021-06-17 14:42:41 +0800

    add user permission for profiles and add search filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -8,6 +8,11 @@ from rest_framework import viewsets
 
 from profiles_api import models
 
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
+
+from rest_framework import filters
+
 class HelloAPIView(APIView):
     """Test api view"""
     serializer_class = serializers.HelloSerializer
@@ -92,5 +97,11 @@ class HelloViewsets(viewsets.ViewSet):
         return Response({'http_method':'DELETE'})
 
 class UserProfilesViewSets(viewsets.ModelViewSet):
+    """handle creating and updating user profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email')
