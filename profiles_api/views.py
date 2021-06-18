c91d6205cf2039f144cf49f8045236f20a06d96f
WilliamWYK <coold030b@gmail.com>
2021-06-18 17:31:30 +0800

    change import layout

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,22 +1,20 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
 from rest_framework import status
-from profiles_api import serializers
-
 from rest_framework import viewsets
-
-from profiles_api import models
-
-from rest_framework.authentication import TokenAuthentication
-from profiles_api import permissions
-
 from rest_framework import filters
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
+
 class HelloAPIView(APIView):
     """Test api view"""
     serializer_class = serializers.HelloSerializer
